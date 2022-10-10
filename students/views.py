b43211c196be0e5b3bc75f02ec44cab6d4798934
Koen Schipper <koen.schipper@me.com>
2022-10-10 20:06:41 +0200

    changes

--- students/views.py
+++ students/views.py
@@ -1,7 +1,9 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
+
 from .models import Student
+from .forms import StudentForm
 
 # Create your views here.
 def index(request):
@@ -12,3 +14,33 @@ def index(request):
 def view_student(request, id):
     student = Student.objects.get(pk=id)
     return HttpResponseRedirect(reverse('index'))
+
+def add_student(request):
+    if request.method == 'POST':
+        form = StudentForm(request.POST)
+        if form.is_valid():
+            new_student_number = form.cleaned_data['student_number']
+            new_first_name = form.cleaned_data['first_name']
+            new_last_name = form.cleaned_data['last_name']
+            new_email = form.cleaned_data['email']
+            new_field_of_study = form.cleaned_data['field_of_study']
+            new_gpa = form.cleaned_data['gpa']
+
+            new_student = Student(
+                student_number = new_student_number,
+                first_name = new_first_name,
+                last_name = new_last_name,
+                email = new_email,
+                field_of_study = new_field_of_study,
+                gpa = new_gpa
+            )
+            new_student.save()
+            return render(request, 'students/add_student.html', {
+                'form': StudentForm(),
+                'success': True
+            })
+    else:
+        form = StudentForm()
+    return render(request, 'students/add_student.html', {
+        'form': StudentForm()
+    })
